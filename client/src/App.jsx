b1434d2a9c20656d1d4de272ff7abe51725c8f10
Arūnas Mikalajūnas <mikalajunasar@gmail.com>
2023-09-19 10:29:17 +0300

    Create layouts, Header, Footer and pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,34 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { ContextWrapper } from "./context/GlobalContext";
+import { PublicLayout } from './layout/PublicLayout';
+import { UserLayout } from './layout/UserLayout';
+import { Home } from './pages/Home';
+import { Register } from './pages/Register';
+import { Login } from './pages/Login';
+import { NoPage } from './pages/NoPage';
+import { List } from './pages/List';
+
 
 function App() {
   return (
-    <div>
-        APP CONTENT
-    </div>
+    <ContextWrapper>
+      <BrowserRouter>
+        <Routes>
+          <Route Component={PublicLayout}>
+            <Route index path='/' element={<Home />}></Route>
+            <Route path='/list' element={<List />}></Route>
+            <Route path='/register' element={<Register />}></Route>
+            <Route path='/login' element={<Login />}></Route>
+          </Route>
+          <Route Component={UserLayout}>
+        
+          </Route>
+          <Route Component={PublicLayout}>
+            <Route path='*' element={<NoPage />}></Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </ContextWrapper>
   );
 }
 
---
+++ client/src/pages/List.jsx
@@ -0,0 +1,47 @@
+import { Link } from "react-router-dom"
+
+export function List () {
+    return (
+    <div className="container px-4 py-3" id="featured-3">
+    <h2 className="pb-3 border-bottom">List page</h2>
+    <div className="row g-4 py-3 row-cols-1 row-cols-lg-3">
+        <div className="feature col">
+            <div className="feature-icon d-inline-flex align-items-center justify-content-center text-bg-primary bg-gradient fs-2 mb-3"></div>
+            <h3 className="fs-2 text-body-emphasis">List content</h3>
+            <p>Paragraph of text beneath the heading to explain the heading</p>
+            <Link to="/" className="btn btn-success rounded-pill py-2">Update</Link>
+        </div>
+        <div className="feature col">
+            <div className="feature-icon d-inline-flex align-items-center justify-content-center text-bg-primary bg-gradient fs-2 mb-3"></div>
+            <h3 className="fs-2 text-body-emphasis">List content</h3>
+            <p>Paragraph of text beneath the heading to explain the heading</p>
+            <Link to="/" className="btn btn-success rounded-pill py-2">Update</Link>
+        </div>
+        <div className="feature col">
+            <div className="feature-icon d-inline-flex align-items-center justify-content-center text-bg-primary bg-gradient fs-2 mb-3"></div>
+            <h3 className="fs-2 text-body-emphasis">List content</h3>
+            <p>Paragraph of text beneath the heading to explain the heading</p>
+            <Link to="/" className="btn btn-success rounded-pill py-2">Update</Link>
+        </div>
+        <div className="feature col">
+            <div className="feature-icon d-inline-flex align-items-center justify-content-center text-bg-primary bg-gradient fs-2 mb-3"></div>
+            <h3 className="fs-2 text-body-emphasis">List content</h3>
+            <p>Paragraph of text beneath the heading to explain the heading</p>
+            <Link to="/" className="btn btn-success rounded-pill py-2">Update</Link>
+        </div>
+        <div className="feature col">
+            <div className="feature-icon d-inline-flex align-items-center justify-content-center text-bg-primary bg-gradient fs-2 mb-3"></div>
+            <h3 className="fs-2 text-body-emphasis">List content</h3>
+            <p>Paragraph of text beneath the heading to explain the heading</p>
+            <Link to="/" className="btn btn-success rounded-pill py-2">Update</Link>
+        </div>
+        <div className="feature col">
+            <div className="feature-icon d-inline-flex align-items-center justify-content-center text-bg-primary bg-gradient fs-2 mb-3"></div>
+            <h3 className="fs-2 text-body-emphasis">List content</h3>
+            <p>Paragraph of text beneath the heading to explain the heading</p>
+            <Link to="/" className="btn btn-success rounded-pill py-2">Update</Link>
+        </div>
+    </div>
+</div>
+)
+}
